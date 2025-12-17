4bc54a80d62fa5c500953867d9769f8aa8378316
mtriza <muhamadtaufiqriza@unida.gontor.ac.id>
2025-12-17 15:35:31 +0800

    fix(opac): Add cache control meta tags
    Prevents browser from caching page without CSS resources
    This should fix login page not displaying styles after session expired redirect

--- resources/views/components/opac/layout.blade.php
+++ resources/views/components/opac/layout.blade.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Expires" content="0">
     <title>{{ $title ?? 'OPAC' }} - Perpustakaan UNIDA Gontor</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
